bec8ea41beacef92e1ea8d05ce8faf8d74c5879c
DashiellMR <dashiellrich@gmail.com>
2024-01-27 16:33:59 -0500

    added proper welcome page

--- wrapped/mysite/settings.py
+++ wrapped/mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ["10.122.71.95", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
