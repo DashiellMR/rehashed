043938c84d25bab5489d15a742a15e760f386b6f
DashiellMR <dashiellrich@gmail.com>
2024-01-27 15:57:15 -0500

    added account page

--- wrapped/account/views.py
+++ wrapped/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
- def account_page(request):
-     return render(request, 'account/account.html')
+def account_page(request):
+    return render(request, 'account/account.html')
