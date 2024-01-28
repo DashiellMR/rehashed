f59ffeb22e7969c86cafdaaa22a85adc32d76737
DashiellMR <dashiellrich@gmail.com>
2024-01-27 19:00:43 -0500

    still reworking login

--- wrapped/new_account/forms.py
+++ wrapped/new_account/forms.py
@@ -9,10 +9,26 @@ class UserAccountForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
+        
     def save(self, commit=True):
         user = super().save(commit=False)
+        phone_number = self.cleaned_data['phone_number']
+        if UserProfile.objects.filter(phone_number=phone_number).exists():
+            raise forms.ValidationError("Phone number is already in use.")
         if commit:
             user.save()
             UserProfile.objects.create(user=user, phone_number=self.cleaned_data['phone_number'])
         return user
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already in use.")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username is already in use.")
+        return username
+    
