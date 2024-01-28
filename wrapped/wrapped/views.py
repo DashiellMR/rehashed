85bb816695033e2d7f586e6e1af9d56575f3fb2c
DashiellMR <dashiellrich@gmail.com>
2024-01-28 00:11:08 -0500

    i'm going to bed

--- wrapped/wrapped/views.py
+++ wrapped/wrapped/views.py
@@ -5,19 +5,13 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-def wrapped_page(request):
-    # No context is passed for now, but you can add context data if needed
-    return render(request, 'wrapped/wrapped.html')
-
 @login_required
 def wrapped_view(request):
     user = request.user
     user_profile = UserProfile.objects.get(user=user)  # Adjust if UserProfile is accessed differently
 
-    # Split categories into a list if they exist, else provide an empty list
     categories = user_profile.categories.split(',') if user_profile.categories else []
-    print(categories)
-    return render(request, 'wrapped.html', {
+    return render(request, 'wrapped/wrapped.html', {
         'username': user.username,
         'email': user.email,
         'categories': categories
