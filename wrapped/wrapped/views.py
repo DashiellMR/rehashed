1b2736ed2ac8e67ab293ea04c0efb48b4dace446
Sebastien Chow <svchow2003@gmail.com>
2024-01-28 02:33:35 -0500

    fields are now charfields

--- wrapped/wrapped/views.py
+++ wrapped/wrapped/views.py
@@ -9,11 +9,14 @@ from django.contrib import messages
 def wrapped_view(request):
     user = request.user
     user_profile = UserProfile.objects.get(user=user)  # Adjust if UserProfile is accessed differently
-    user_tears = user_profile.tearsq1
+    user_tears1 = user_profile.tearsq1
+    user_party1 = user_profile.partyq1
+
     categories = user_profile.categories.split(',') if user_profile.categories else []
     return render(request, 'wrapped/wrapped.html', {
         'username': user.username,
         'email': user.email,
         'categories': categories,
-        'tears': user_tears
+        'tears': user_tears1,
+        'party': user_party1
     })
