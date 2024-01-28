64729b1c754080310b3461358d20451c8c354c29
DashiellMR <dashiellrich@gmail.com>
2024-01-27 22:05:14 -0500

    login page added

--- wrapped/login/urls.py
+++ wrapped/login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import login
+from .views import login_view, success_view
 
 urlpatterns = [
-    path("", login, name='login'),
+    path("login/", login_view, name='login'),
+    path("success/", success_view, name='success_page'),
+    # ... other URL patterns ...
 ]
