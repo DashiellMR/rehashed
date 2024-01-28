0787727d116508c939c50a68bcea1cbbd06f008c
Sebastien Chow <svchow2003@gmail.com>
2024-01-28 01:15:32 -0500

    add wrapped and entry button to account

--- wrapped/journal/views.py
+++ wrapped/journal/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from new_account.models import UserProfile
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-def journal_page(request):  
-    return render(request, 'journal/journal.html',)
+@login_required
+def journal_page(request):
+    user = request.user
+    user_profile = UserProfile.objects.get(user=user)  # Adjust if UserProfile is accessed differently
+
+    categories = user_profile.categories.split(',') if user_profile.categories else []
+    return render(request, 'journal/journal.html', {
+        'username': user.username,
+        'email': user.email,
+        'categories': categories
+    })
