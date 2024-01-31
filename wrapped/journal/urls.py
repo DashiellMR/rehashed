6f2cc18d74b2631a194b4b30c5a601516a372cbe
DashiellMR <dashiellrich@gmail.com>
2024-01-31 04:13:55 -0500

    fixed 4am blunder

--- wrapped/journal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import journal_page, form_data
-
-app_name = 'journal'
-urlpatterns = [
-    path("", journal_page, name='journal'),
-    path("form_data/", form_data, name='form_data')
-]
--- wrapped/welcome/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import welcome_page, register_account, success
-
-app_name="welcome"
-urlpatterns = [
-    path("", welcome_page, name='welcome'),
-    path('register/', register_account, name='register'),
-    path('success/', success, name='success')
-]
